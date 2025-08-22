36917e20bbde24fa524d47bc4d1077d7cd8d006f
stlawton <lawtons347@gmail.com>
2025-08-22 11:16:10 -0400

    Added print_sudoku function

--- src/sudoku_print_and_add.py
+++ src/sudoku_print_and_add.py
@@ -1,1 +1,19 @@
-# Write your solution here
+def print_sudoku(sudoku: list):
+  row = 0
+  while row < 9:
+    column = 0
+    while column < 9:
+      if sudoku[row][column] == 0:
+        if column % 3 == 0:
+          print("  _", end="")
+        else:
+          print(" _", end="")
+      else:
+        print(sudoku[row][column])
+      column += 1
+    print()
+    if row == 2 or row == 5:
+      print()
+    row += 1
+    
+        
