6b7e80501e2b6dc9d235aec15b41a0092a04a686
stlawton <lawtons347@gmail.com>
2025-08-22 12:04:45 -0400

    Created add_number function
    Fixed some issues with print_sudoku spaces

--- src/sudoku_print_and_add.py
+++ src/sudoku_print_and_add.py
@@ -4,16 +4,18 @@ def print_sudoku(sudoku: list):
     column = 0
     while column < 9:
       if sudoku[row][column] == 0:
-        if column % 3 == 0:
-          print("  _", end="")
+        if column > 0 and column % 3 == 0:
+          print(" _ ", end="")
         else:
-          print(" _", end="")
+          print("_ ", end="")
       else:
-        print(sudoku[row][column])
+        print(sudoku[row][column], end=" ")
       column += 1
     print()
     if row == 2 or row == 5:
       print()
     row += 1
-    
+
         
+def add_number(sudoku: list, row_no: int, column_no: int, number: int):
+  sudoku[row_no][column_no] = number
